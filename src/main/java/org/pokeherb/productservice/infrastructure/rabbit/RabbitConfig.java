482ec8c3d8e840fee3a52843b3904fe65b0b815c
RCNR <iop04226@gmail.com>
2025-11-25 17:36:11 +0900

    hotifx: rabbitMQ 관련 설정 수정

--- src/main/java/org/pokeherb/productservice/infrastructure/rabbit/RabbitConfig.java
+++ src/main/java/org/pokeherb/productservice/infrastructure/rabbit/RabbitConfig.java
@@ -1,13 +1,31 @@
 package org.pokeherb.productservice.infrastructure.rabbit;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(RabbitProducer.class)
+@EnableConfigurationProperties(RabbitProductProperties.class)
 public class RabbitConfig {
 
-    private final RabbitProducer productProperties;
+    private final RabbitProductProperties productProperties;
+
+    @Bean
+    public TopicExchange productExchange() {
+        return new TopicExchange(productProperties.exchange(), true, false);
+    }
+
+    @Bean
+    public Queue productQueue() {
+        return QueueBuilder.durable(productProperties.queue()).build();
+    }
+
+    @Bean
+    public Binding productBinding(Queue hubQueue, TopicExchange productExchange) {
+        return BindingBuilder.bind(hubQueue).to(productExchange)
+                .with(productProperties.routingKey());
+    }
 }
--- src/main/java/org/pokeherb/productservice/infrastructure/rabbit/RabbitProductProperties.java
+++ src/main/java/org/pokeherb/productservice/infrastructure/rabbit/RabbitProductProperties.java
@@ -2,7 +2,7 @@ package org.pokeherb.productservice.infrastructure.rabbit;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "rabbit.product-service")
+@ConfigurationProperties(prefix = "rabbit.product")
 public record RabbitProductProperties(
         String exchange,
         String queue,
